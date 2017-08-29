32c015b598f64baf40a93f1849e1c7e74ec62e0f
Prince Kumar <prince.m.singh11@gmail.com>
2017-08-29 19:02:47 +0530

    Task 3: Network (Progress)

--- FoodNeturationChecker/app/src/main/java/com/kumar/prince/foodneturationchecker/communication/FC_OpenFoodFactsAPIClient.java
+++ FoodNeturationChecker/app/src/main/java/com/kumar/prince/foodneturationchecker/communication/FC_OpenFoodFactsAPIClient.java
@@ -1,5 +1,20 @@
 package com.kumar.prince.foodneturationchecker.communication;
 
+import android.support.annotation.NonNull;
+
+import com.kumar.prince.foodneturationchecker.Error.FC_ServerUnreachableException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
  *  Created by prince on 29/8/17.
  * */
@@ -7,4 +22,39 @@ package com.kumar.prince.foodneturationchecker.communication;
 public class FC_OpenFoodFactsAPIClient {
 
 
+    public static final String ENDPOINT_BARCODE = "http://world.openfoodfacts.org/api/v0/product/";
+    public static final String ENDPOINT_SEARCH = "http://world.openfoodfacts.org/cgi/";
+
+    private final OpenFoodFactsApi mOpenFoodFactsAPI;
+
+    public FC_OpenFoodFactsAPIClient(@NonNull String endpoint) {
+        checkNotNull(endpoint);
+
+        this.mOpenFoodFactsAPI = new Retrofit.Builder()
+                .baseUrl(endpoint)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(getOkHttpClient())
+                .build()
+                .create(OpenFoodFactsApi.class);
+    }
+
+    private OkHttpClient getOkHttpClient() {
+        try {
+
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+
+            final HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+            httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            builder.addInterceptor(httpLoggingInterceptor);
+
+            return builder.build();
+        } catch (Exception e) {
+            throw new FC_ServerUnreachableException();
+        }
+    }
+
+
+    interface OpenFoodFactsApi {
+
+    }
 }
