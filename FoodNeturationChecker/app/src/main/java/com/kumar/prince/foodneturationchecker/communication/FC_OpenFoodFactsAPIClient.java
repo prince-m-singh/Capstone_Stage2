1819fd897c791da0c8d983699144b0872929b79f
Prince Kumar <prince.m.singh11@gmail.com>
2017-08-30 20:49:32 +0530

    Task 3: Network (Progress)

--- FoodNeturationChecker/app/src/main/java/com/kumar/prince/foodneturationchecker/communication/FC_OpenFoodFactsAPIClient.java
+++ FoodNeturationChecker/app/src/main/java/com/kumar/prince/foodneturationchecker/communication/FC_OpenFoodFactsAPIClient.java
@@ -11,6 +11,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -57,6 +58,16 @@ public class FC_OpenFoodFactsAPIClient {
         return this.mOpenFoodFactsAPI.getProduct(barcode);
     }
 
+    public Call<FC_Search> getProducts(@NonNull String categoryKey,
+                                       @NonNull String nutritionGradeValue) {
+        return this.mOpenFoodFactsAPI.getProducts(categoryKey, nutritionGradeValue);
+    }
+
+    public Call<FC_Search> getCountryCategory(@NonNull final String categoryKey, @NonNull final String countryKey) {
+        return this.mOpenFoodFactsAPI.getCountryCategory(categoryKey, countryKey);
+    }
+
+
 
     interface OpenFoodFactsApi {
 
@@ -66,5 +77,20 @@ public class FC_OpenFoodFactsAPIClient {
                 @Path("barcode") String barcode
         );
 
+        @GET("search.pl?action=process&tagtype_0=categories&tag_contains_0=contains&tagtype_1=nutrition_grades&tag_contains_1=contains&sort_by=unique_scans_n&page_size=20&page=1&json=1")
+        Call<FC_Search> getProducts(
+                @Query("tag_0") String categoryKey,
+                @Query("tag_1") String nutritionGrade
+        );
+
+
+        //Example : http://world.openfoodfacts.org/cgi/search.pl?action=process&tagtype_0=categories&tag_contains_0=contains&tag_0=cheese&tagtype_1=countries&tag_contains_1=contains&tag_1=france&tagtype_2=nutrition_grades&tag_contains_2=does_not_contain&tag_2=unknown&sort_by=unique_scans_n&page_size=5&page=1&json=1
+        @GET("search.pl?action=process&tagtype_0=categories&tag_contains_0=contains&tagtype_1=countries&tag_contains_1=contains&tagtype_2=nutrition_grades&tag_contains_2=does_not_contain&tag_2=unknown&sort_by=unique_scans_n&page_size=5&page=1&json=1")
+        Call<FC_Search> getCountryCategory(
+                @Query("tag_0") String categoryKey,
+                @Query("tag_1") String countryKey
+        );
     }
+
+
 }
---
+++ FoodNeturationChecker/app/src/main/java/com/kumar/prince/foodneturationchecker/communication/FC_Search.java
@@ -0,0 +1,77 @@
+package com.kumar.prince.foodneturationchecker.communication;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import com.kumar.prince.foodneturationchecker.data.model.FC_Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by prince on 30/8/17.
+ *
+ */
+public class FC_Search {
+
+    @SerializedName("page_size")
+    @Expose
+    private String page_size;
+    @SerializedName("FCProducts")
+    @Expose
+    private List<FC_Product> FCProducts = new ArrayList<FC_Product>();
+    @SerializedName("page")
+    @Expose
+    private String page;
+    @SerializedName("count")
+    @Expose
+    private Integer count;
+    @SerializedName("skip")
+    @Expose
+    private Integer skip;
+
+    /**
+     * 
+     * @return
+     *     The page_size
+     */
+    public String getPage_size() {
+        return page_size;
+    }
+
+    /**
+     * 
+     * @return
+     *     The FCProducts
+     */
+    public List<FC_Product> getFCProducts() {
+        return FCProducts;
+    }
+
+    /**
+     * 
+     * @return
+     *     The page
+     */
+    public String getPage() {
+        return page;
+    }
+
+    /**
+     * 
+     * @return
+     *     The count
+     */
+    public Integer getCount() {
+        return count;
+    }
+
+    /**
+     * 
+     * @return
+     *     The skip
+     */
+    public Integer getSkip() {
+        return skip;
+    }
+
+}
