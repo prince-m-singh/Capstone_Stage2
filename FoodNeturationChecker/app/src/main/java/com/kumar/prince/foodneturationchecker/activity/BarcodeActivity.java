1b73e0c2e5fc30d5e9bf2db4f4c1af05cfa3699d
Prince Kumar <prince.m.singh11@gmail.com>
2017-09-03 12:05:33 +0530

    Task 4: Barcode scanner(progress)

--- FoodNeturationChecker/app/src/main/java/com/kumar/prince/foodneturationchecker/activity/BarcodeActivity.java
+++ FoodNeturationChecker/app/src/main/java/com/kumar/prince/foodneturationchecker/activity/BarcodeActivity.java
@@ -2,6 +2,7 @@ package com.kumar.prince.foodneturationchecker.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,8 @@ import com.kumar.prince.foodneturationchecker.Error.FC_ServerUnreachableExceptio
 import com.kumar.prince.foodneturationchecker.R;
 import com.kumar.prince.foodneturationchecker.communication.FC_OpenFoodFactsAPIClient;
 import com.kumar.prince.foodneturationchecker.communication.FC_ProductBarcode;
+import com.kumar.prince.foodneturationchecker.data.callbackinterface.FC_ProductSourceInterface;
+import com.kumar.prince.foodneturationchecker.data.model.FC_Product;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -26,7 +29,7 @@ import static com.kumar.prince.foodneturationchecker.activity.ScanBarcodeActivit
  * Created by prince on 2/9/17.
  */
 
-public class BarcodeActivity extends AppCompatActivity {
+public class BarcodeActivity extends AppCompatActivity implements FC_ProductSourceInterface{
 
     TextView tvResult;
     Button btnScanner;
@@ -62,7 +65,17 @@ public class BarcodeActivity extends AppCompatActivity {
                 if(data!=null){
                     Barcode barcode = data.getParcelableExtra(GETRESULT);
                     tvResult.setText(barcode.displayValue);
-                    barcodeRequest(barcode.displayValue);
+                    getProduct(barcode.displayValue, new GetProductCallback() {
+                        @Override
+                        public void onProductLoaded(FC_Product FCProduct) {
+                            tvResult.append(FCProduct.toString());
+                        }
+
+                        @Override
+                        public void onError(Throwable throwable) {
+                            tvResult.append(throwable.getMessage());
+                        }
+                    });
                 }
             }
         }
@@ -72,7 +85,9 @@ public class BarcodeActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
     }
 
-    private void barcodeRequest(String barcode){
+
+    @Override
+    public void getProduct(@NonNull String barcode, @NonNull GetProductCallback getProductCallback) {
         FC_OpenFoodFactsAPIClient FCOpenFoodFactsAPIClient = new FC_OpenFoodFactsAPIClient(FC_OpenFoodFactsAPIClient.ENDPOINT_BARCODE);
         Call<FC_ProductBarcode> call = FCOpenFoodFactsAPIClient.getProduct(barcode);
 
@@ -80,9 +95,11 @@ public class BarcodeActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<FC_ProductBarcode> call, Response<FC_ProductBarcode> response) {
 
-                FC_ProductBarcode fc_productBarcodegoit  = response.body();
+                FC_ProductBarcode fc_productBarcode = response.body();
+                FC_Product fc_product=fc_productBarcode.getFCProduct();
 
-                Timber.d(response.message()+" "+fc_productBarcode.toString());
+                Timber.d(response.message()+" "+fc_product.toString());
+                getProductCallback.onProductLoaded(fc_product);
 
             }
 
@@ -90,9 +107,15 @@ public class BarcodeActivity extends AppCompatActivity {
             public void onFailure(Call<FC_ProductBarcode> call, Throwable t) {
                 FC_ServerUnreachableException e = new FC_ServerUnreachableException();
                 Timber.d(e.getMessage());
+                getProductCallback.onError(e);
 
             }
         });
+    }
+
+    @Override
+    public void getProducts(@NonNull String categoryKey, @NonNull String nutritionGradeValue, @NonNull GetProductsCallback getProductsCallback) {
+
 
     }
 }
